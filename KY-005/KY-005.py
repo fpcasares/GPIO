6b9c56b27701ca387b676bebed6096d19eb7d2b4
Facundo Pablo Casares <fpcasares@gmail.com>
2020-07-04 12:18:01 -0300

    Create KY-005.py

---
+++ KY-005/KY-005.py
@@ -0,0 +1,9 @@
+#---------------------------------------------------------------------#
+#Name - IR&NECDataSend.py
+#Description - Sends data from the IR sensor but uses the official NEC Protocol (command line version)
+#Date - 07/04/20 - 18/08/19
+#---------------------------------------------------------------------#
+#Imports modules
+import RPi.GPIO as GPIO
+from time import sleep
+from datetime import datetime
