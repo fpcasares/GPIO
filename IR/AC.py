cd90ee4e9e360c8c6293385266b49273984e49bc
Facundo Pablo Casares <fpcasares@gmail.com>
2020-07-09 17:48:11 -0300

    Added AC.py
    Python script to send commands to AC

---
+++ IR/AC.py
@@ -0,0 +1,24 @@
+# Imports
+
+import argparse
+import configparser
+from IREMIT import *
+
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--debug", help="Show debugging information", action="store_true")
+    parser.add_argument("-c","--command",type=str, help="Command to send to AC")
+    args = parser.parse_args()
+    
+    if not args.command:
+        command='on'
+
+
+
+
+if __name__ == '__main__':
+    main()
+       
+    
