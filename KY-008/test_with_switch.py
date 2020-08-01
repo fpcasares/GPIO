e51ed0870b21f6a0a0ade4627e485db8efe52d6a
fpcasares@gmail.com <fpcasares@gmail.com>
2020-08-01 22:58:10 +0000

    added with switch

--- KY-008/test_with_switch.py
+++ KY-008/test_with_switch.py
@@ -1,44 +1,35 @@
-# needed modules will be imported
 import RPi.GPIO as GPIO
 import time
-  
-GPIO.setmode(GPIO.BCM)
-  
-# The input pin of the Sensor will be declared. Additional to that the pull-up resistor will be activated.
-GPIO_PIN_OUT=14
+
+#GPIO Setup   
+GPIO_14="cable verde"
+GPIO_15="cable rojo"
+VCC="cable amarillo"
+GND="cable naranja"
+
+
 GPIO_PIN_IN = 15
-GPIO.setup(GPIO_PIN, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO_PIN_OUT = 14
+
+GPIO.setmode(GPIO.BCM)
+
 GPIO.setup(GPIO_PIN_OUT, GPIO.OUT)
+GPIO.setup(GPIO_PIN_IN, GPIO.IN)
 
 
-on_status=False
 
 
-def ON():
-    print("Signal detected")
-    if not on_status:
-        GPIO.output(GPIO_PIN_OUT, True)
-        on_status=True
-    if on_status:
-        GPIO.output(GPIO_PIN_OUT, False)
-        on_status=False
-    
-    
-  
-print "Sensor-Test [press ctrl+c to end it]"
-  
-# This output function will be started at signal detection.
+def intermitent():
+    while True:
+        if GPIO.input(GPIO_PIN_IN):
+            GPIO.output(GPIO_PIN_OUT, True)
+        else:
+            GPIO.output(GPIO_PIN_OUT, False)
 
-        
-  
-# At the moment of detecting a Signal ( falling signal edge ) the output function will be activated.
-GPIO.add_event_detect(GPIO_PIN_IN, GPIO.FALLING, callback=ON, bouncetime=100) 
-  
-# main program loop
-try:
-        while True:
-                time.sleep(1)
-  
-# Scavenging work after the end of the program
-except KeyboardInterrupt:
-        GPIO.cleanup()
+
+
+if __name__=='__main__':
+    intermitent()
+    GPIO.cleanup()
+
+
