004d541daef6b63f6529e1c8c00a6f63bc35f757
Facundo Pablo Casares <fpcasares@gmail.com>
2020-08-01 18:48:43 -0300

    Update test.py

--- KY-008/test.py
+++ KY-008/test.py
@@ -1,20 +1,27 @@
 import RPi.GPIO as GPIO
 
- #GPIO Setup   
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(14, GPIO.OUT)
-GPIO.output(14, True)
-
-
-while not KeyboardInterrupt:
-    GPIO.output(14, True)
-    time.sleep(1)
-    GPIO.output(14, False)
-    time.sleep(1)
-
-GPIO.cleanup()
-
+#GPIO Setup   
 GPIO_14="cable verde"
 GPIO_15="cable rojo"
 VCC="cable amarillo"
 GND="cable naranja"
+
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(14, GPIO.OUT)
+GPIO.output(14, True)
+
+def intermitent():
+    while not KeyboardInterrupt:
+        GPIO.output(14, True)
+        time.sleep(1)
+        GPIO.output(14, False)
+        time.sleep(1)
+
+
+if __name__=='__main__':
+    intermitent()
+    GPIO.cleanup()
+
+
