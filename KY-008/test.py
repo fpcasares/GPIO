d48403d3ea5a63490e6ad76f9c42deb3c1110db0
Facundo Pablo Casares <fpcasares@gmail.com>
2020-08-01 18:40:14 -0300

    Create test.py

---
+++ KY-008/test.py
@@ -0,0 +1,20 @@
+import RPi.GPIO as GPIO
+
+ #GPIO Setup   
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(14, GPIO.OUT)
+GPIO.output(14, True)
+
+
+while not KeyboardInterrupt:
+    GPIO.output(14, True)
+    time.sleep(1)
+    GPIO.output(14, False)
+    time.sleep(1)
+
+
+
+GPIO_14="cable verde"
+GPIO_15="cable rojo"
+VCC="cable amarillo"
+GND="cable naranja"
